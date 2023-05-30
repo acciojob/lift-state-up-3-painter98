01bfb5d90eaeb92b54221cb1767c2e0f58be7713
painter98 <emailmepallavi@gmail.com>
2023-05-31 00:02:28 +0530

    chang2

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ const App = () => {
 
   return (
     <div className="parent">
+      <h1>Parent Component</h1>
         {/* Do not remove the main div */}
         <Option text={'Child Component 1'} state={setOption} option={'Option 1'}/>
         <Option text={'Child Component 2'} state={setOption} option={'Option 2'}/>
